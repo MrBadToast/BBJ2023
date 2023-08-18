9d8b0b8e6d003685cd5d9f7a89b7a818a507b85e
zerosouth <kyns320@gmail.com>
2023-08-19 02:45:47 +0900

    [fix] 템플릿 세팅 간 미사용 로직 삭제

--- Assets/Scripts/UI/Popup/UISettingPopup.cs
+++ Assets/Scripts/UI/Popup/UISettingPopup.cs
@@ -5,49 +5,9 @@ using UnityEngine.UI;
 
 public class UISettingPopup : UIBasePopup
 {
-    [System.Serializable]
-    public class ContentGroup
-    {
-        public GameObject content;
-
-        public void SetActive(bool isActive)
-        {
-            content.SetActive(isActive);
-        }
-    }
-
-    [SerializeField]
-    private string defaultViewCategory;
-
-    private string currentViewCategory;
-
-    [SerializeField]
-    private SerializableDictionary<string, ContentGroup> contentGroups;
-
     public override void Init(UIData uiData)
     {
-        foreach (var group in contentGroups.Values)
-        {
-            group.SetActive(false);
-        }
 
-        ChangeCategory(defaultViewCategory);
-    }
-
-    public void OnSelectCategory(string category)
-    {
-        ChangeCategory(category);
-    }
-
-    private void ChangeCategory(string category)
-    {
-        if (!string.IsNullOrEmpty(currentViewCategory))
-        {
-            contentGroups[currentViewCategory].SetActive(false);
-        }
-
-        contentGroups[category].SetActive(true);
-        currentViewCategory = category;
     }
 
     public override void Close()
@@ -55,10 +15,4 @@ public class UISettingPopup : UIBasePopup
         SaveLoadSystem.Instance.Save();
         base.Close();
     }
-
-    public override void BeginClose()
-    {
-        base.BeginClose();
-    }
-
 }
