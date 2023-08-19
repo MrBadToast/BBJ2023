a368d002375f0933cf2faf0decdd4437fe78ae8d
zerosouth <kyns320@gmail.com>
2023-08-19 16:29:38 +0900

    [feat] 도감창 구현

--- Assets/Scripts/Data/DataContainer/CollectionContainer.cs
+++ Assets/Scripts/Data/DataContainer/CollectionContainer.cs
@@ -12,6 +12,34 @@ public class CollectionContainer : ScriptableObject
     public SerializableDictionary<string, CreatureData> DataTable => dataTable;
 
 
+    [Button("파일 자동 탐색")]
+    private void AutoSetupByName(string nameTag)
+    {
+#if UNITY_EDITOR
+        var findTable = new SerializableDictionary<string, CreatureData>();
+        string[] guids = UnityEditor.AssetDatabase.FindAssets($"{nameTag} t:CreatureData", new[] { "Assets" });
 
+        foreach (string guid in guids)
+        {
+            string assetPath = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
+            var fileName = System.IO.Path.GetFileName(assetPath);
+            var file = (CreatureData)UnityEditor.AssetDatabase.LoadAssetAtPath(assetPath, typeof(CreatureData));
+            findTable.Add(fileName.Replace(".asset", ""), file);
+        }
+
+        dataTable.Clear();
+
+        foreach (var item in findTable)
+        {
+            var key = item.Key.ToString().Replace($"{nameTag}", "");
+            dataTable.Add(key, item.Value);
+        }
+
+        UnityEditor.EditorUtility.SetDirty(this);
+        UnityEditor.AssetDatabase.Refresh();
+        UnityEditor.EditorApplication.RepaintProjectWindow();
+        UnityEditor.EditorApplication.RepaintHierarchyWindow();
+#endif
+    }
 
 }
--- Assets/Scripts/UI/Element/UICollectionListview.cs
+++ Assets/Scripts/UI/Element/UICollectionListview.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UICollectionListview : UIListView<UICollectionSlot>
@@ -7,11 +8,57 @@ public class UICollectionListview : UIListView<UICollectionSlot>
     [SerializeField]
     private CollectionContainer collectionContainer;
 
+    [SerializeField]
+    private GameObject infoPopup;
+    [SerializeField]
+    private UIBaseText infoNameText;
+    private UIBaseText infoDescriptionText;
+    private UIBaseImage infoIconImage;
+
     public void Start()
     {
-        var collectionKeyList = SaveLoadSystem.Instance.SaveLoadData.collectionKeyList;
+        CreateCollectionSlots();
     }
 
 
+    public void CreateCollectionSlots()
+    {
+        var haveKeyList = SaveLoadSystem.Instance.SaveLoadData.collectionKeyList;
+        var keys = new List<string>();
+        foreach (var key in collectionContainer.DataTable.Keys)
+        {
+            keys.Add(key);
+        }
 
+        foreach (var haveKey in haveKeyList)
+        {
+            keys.Remove(haveKey);
+        }
+
+        foreach (var key in haveKeyList)
+        {
+            var content = AddContent();
+            content.SetData(this, collectionContainer.DataTable[key], true);
+        }
+
+        foreach (var key in keys)
+        {
+            var content = AddContent();
+            content.SetData(this, collectionContainer.DataTable[key], false);
+        }
+    }
+
+    public void ShowInfoPopup(Vector3 position, CreatureData creatureData)
+    {
+        infoNameText.SetText(creatureData.CreatureName);
+        infoDescriptionText.SetText(creatureData.Context);
+        infoIconImage.SetImage(creatureData.Icon);
+
+        infoPopup.SetActive(true);
+    }
+
+    public void HideInfoPopup()
+    {
+        infoPopup.SetActive(false);
+    }
 }
