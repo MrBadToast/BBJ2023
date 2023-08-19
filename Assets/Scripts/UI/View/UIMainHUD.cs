67d9aedf1f4193110767d1d80b5c58afcd69f5fd
zerosouth <kyns320@gmail.com>
2023-08-19 18:21:35 +0900

    [feat] 인게임 UI 업데이트

--- Assets/Scripts/UI/View/UIMainHUD.cs
+++ Assets/Scripts/UI/View/UIMainHUD.cs
@@ -1,10 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIMainHUD : UIBaseView
 {
+    private WorldController worldController;
     UIPausePopup pausePopup;
+
+    [SerializeField]
+    private UIBaseGauge surfaceGauge;
+    [SerializeField]
+    private UIBaseGauge humidityGauge;
+    [SerializeField]
+    private UIBaseGauge temperatureGauge;
+
+    private void Awake()
+    {
+        worldController = FindObjectOfType<WorldController>();
+        worldController.updateStatusEvent.AddListener(UpdateStatus);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
 
     public override void Init(UIData uiData)
     {
@@ -22,6 +42,13 @@ public class UIMainHUD : UIBaseView
                 pausePopup = null;
             });
         }
+    }
+
+    public void UpdateStatus(StatusInfo statusInfo)
+    {
+        surfaceGauge.UpdateGauge((statusInfo.GetElement(StatusType.Surface).CalculateTotalAmount() + 1) * 0.5f);
+        humidityGauge.UpdateGauge(statusInfo.GetElement(StatusType.Humidity).CalculateTotalAmount());
+        temperatureGauge.UpdateGauge((statusInfo.GetElement(StatusType.Temperature).CalculateTotalAmount() + 1) * 0.5f);
     }
 
 }
